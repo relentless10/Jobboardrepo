47e08a7187fa640c09281c30aa5e6381c50e4fb5
relentless10 <wilfredaikpokpegbe@gmail.com>
2026-06-04 00:52:00 +0100

    fix cors for netlify

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const express = require("express");
 const app = express();
 
 app.use(express.json());
-app.use(require("cors")());
+app.use(require("cors")({
+origin: "https://euphonious-selkie-ba2993.netlify.app"
+}));
 
 const jobsRouter = require("./routes/jobs");
 app.use("/jobs", jobsRouter);
