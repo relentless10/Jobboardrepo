4b0f4abaec077e99822047f6d89eb8fbc531fc33
relentless10 <wilfredaikpokpegbe@gmail.com>
2026-06-04 03:13:16 +0100

    fix db pool order

--- db.js
+++ db.js
@@ -1,5 +1,7 @@
-const { Pool } = require("pg");
 require("dotenv").config();
+const { Pool } = require("pg");
+
+console.log("Creating pool with:", process.env.DATABASE_URL);
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
